bc132c6e37ba7b7533d2517a5691dfe7b5876d2d
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-15 21:57:13 -0300

    chore: removing all the default code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// Global Styles
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// Metadata
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
