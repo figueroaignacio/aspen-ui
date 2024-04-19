c02226f6e3758237dddacdd4b1ad989d2f14d729
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-19 17:56:32 -0300

    feat: adding site config

---
+++ config/site.ts
@@ -0,0 +1,21 @@
+export const siteConfig = Object.freeze({
+  title: "nio/ui",
+  author: {
+    name: "Ignacio Figueroa",
+    url: "https://github.com/ignaciofigueroadev",
+  },
+  creator: "Ignacio Figueroa",
+  description: "lorem",
+  ogImage: "",
+  url: "https://ui-nio.vercel.app/",
+  keywords: [
+    "UI",
+    "UI Kit",
+    "UI Library",
+    "React UI Library",
+    "Next UI Library",
+    "Astro UI Library",
+    "User Interface Kit",
+    "Nio",
+  ],
+});
