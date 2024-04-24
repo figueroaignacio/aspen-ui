730be81ffc66b6d113483ba47469cbfd09ea54b4
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-24 19:34:06 -0300

    chore: removing metadata of the layout of lobby group

--- app/(lobby)/layout.tsx
+++ app/(lobby)/layout.tsx
@@ -8,45 +8,6 @@ import { ThemeProvider } from "next-themes";
 import "@/styles/globals.css";
 
 // Config
-import { siteConfig } from "@/config/site";
-
-// Metadata
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(
-    "https://icoded.vercel.app/" ?? "http://localhost:3000"
-  ),
-  title: {
-    default: siteConfig.title,
-    template: ` - %s ${siteConfig.title}`,
-  },
-  authors: [
-    {
-      name: siteConfig.author.name,
-      url: siteConfig.author.url,
-    },
-  ],
-  creator: siteConfig.creator,
-  description: siteConfig.description,
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.title,
-    description: siteConfig.description,
-    siteName: siteConfig.title,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.title,
-      },
-    ],
-  },
-  keywords: siteConfig.keywords,
-};
 
 export default function LobbyLayout({
   children,
