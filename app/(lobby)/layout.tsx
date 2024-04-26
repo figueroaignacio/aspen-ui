0258766ac817989c9764c619249a9955f45d57ca
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-26 18:27:57 -0300

    chore: adding a new prop to the theme provider component

--- app/(lobby)/layout.tsx
+++ app/(lobby)/layout.tsx
@@ -17,6 +17,7 @@ export default function LobbyLayout({
       <body>
         <ThemeProvider
           attribute="class"
+          enableSystem
           defaultTheme="dark"
           disableTransitionOnChange
         >
