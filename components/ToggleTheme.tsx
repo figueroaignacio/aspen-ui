5a6cd394eb880cd96f4c128bb4896e4ebcccfb8c
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-26 17:46:48 -0300

    style: changing the style of icons

--- components/ToggleTheme.tsx
+++ components/ToggleTheme.tsx
@@ -1,30 +1,19 @@
-"use client";
-
 // Hooks
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 // Icons
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
 export const ToggleTheme = () => {
   const { theme, setTheme } = useTheme();
-  const [currentTheme, setCurrentTheme] = useState(theme);
-
-  useEffect(() => {
-    setCurrentTheme(theme);
-  }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = currentTheme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    setCurrentTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <button onClick={toggleTheme}>
-      {currentTheme === "light" ? (
+      {theme === "light" ? (
         <MoonIcon className="h-6 w-6" />
       ) : (
         <SunIcon className="h-6 w-6" />
