44c22f8c06a41947ecc3d6143867d5dd7eb8343d
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-26 23:14:49 -0300

    feat: adding new logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
 export function Logo() {
-  return <Link href={"/"}>nio/ui</Link>;
+  return (
+    <Link href={"/"} className="text-xl font-bold">
+      NeroUI
+    </Link>
+  );
 }
