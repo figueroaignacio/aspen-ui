8fb1759a32bedf101da597c321ba3bdbc28ee8ce
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-28 17:52:56 -0300

    style(components): adding opacity 100 to the logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href={"/"} className="text-xl font-bold">
+    <Link href={"/"} className="text-xl font-bold opacity-100">
       NeroUI
     </Link>
   );
