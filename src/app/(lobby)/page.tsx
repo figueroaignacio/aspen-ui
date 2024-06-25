0c70bc7b7c2b2c60bc9196e8183b580be3d0319b
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-25 18:08:51 -0300

    style: changing some styles in the home page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,9 +1,9 @@
 export default function HomePage() {
   return (
-    <section className="flex flex-col justify-center items-center text-center gap-5 py-56">
+    <section className="flex flex-col justify-center items-center text-center gap-5 py-40 lg:max-w-3xl mx-auto">
       <div className="text-pretty">
         <h1 className="text-4xl lg:text-6xl font-bold gradient-text">
-          Simple components to copy and paste
+          Copy and paste modern components
         </h1>
         <p className="text-foreground text-sm">
           Modern, customizable, and simple components to build your system
