3ff1c9c3ef4b1c336a23c4bd3bfbbc4198bdce14
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-27 14:48:43 -0300

    feat: change project name

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,14 +1,14 @@
 export const siteConfig = Object.freeze({
-  title: "NeroUI",
+  title: "aspen/ui",
   author: {
     name: "Ignacio Figueroa",
     url: "https://github.com/figueroaignacio/",
   },
   creator: "Ignacio Figueroa",
   description:
-    "Nero UI is a UI component library specifically designed for developers using Next.js and React. Built with Tailwind CSS, NeroUI offers a modern and highly customizable solution to accelerate the development of attractive and functional user interfaces.",
+    "Nero UI is a UI component library specifically designed for developers using Next.js and React. Built with Tailwind CSS, aspen/ui offers a modern and highly customizable solution to accelerate the development of attractive and functional user interfaces.",
   ogImage: "",
-  url: "https://neroui.vercel.app/",
+  url: "https://aspen/ui.vercel.app/",
   keywords: [
     "UI",
     "UI Kit",
