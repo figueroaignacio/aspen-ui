3fab73e286e92ff1fa8913de6e87c067bed2ab1f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-10-03 17:49:13 -0300

    feat: add mdx styles

--- components/mdx/mdx-components.tsx
+++ components/mdx/mdx-components.tsx
@@ -11,10 +11,7 @@ const useMDXComponents = (code: string) => {
 const components = {
   Image,
   h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1
-      className={cn("mt-2 scroll-m-20 font-bold text-4xl", className)}
-      {...props}
-    />
+    <h1 className={cn("mt-2 scroll-m-20 font-bold", className)} {...props} />
   ),
   h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
     <h2
@@ -36,31 +33,28 @@ const components = {
   ),
   a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
     <a
-      className={cn("font-medium underline underline-offset-4", className)}
+      className={cn("font-medium  underline-offset-4", className)}
       {...props}
     />
   ),
   p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
     <p
-      className={cn("leading-7 my-4 text-base opacity-85", className)}
+      className={cn("leading-7 [&:not(:first-child)]:my-2", className)}
       {...props}
     />
   ),
   ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className={cn("my-6 ml-4 list-disc space-y-2", className)} {...props} />
+    <ul className={cn("my-6 ml-4 list-disc", className)} {...props} />
   ),
   ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol
-      className={cn("my-6 ml-4 list-decimal space-y-2", className)}
-      {...props}
-    />
+    <ol className={cn("my-6 ml-4 list-decimal", className)} {...props} />
   ),
-  li: ({ className, ...props }: React.HTMLAttributes<HTMLLIElement>) => (
+  li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
     <li className={cn("mt-2", className)} {...props} />
   ),
   blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
     <blockquote
-      className={cn("mt-6 border-l-4 pl-6 italic text-gray-600", className)}
+      className={cn("mt-6 border-l-2 pl-6 italic", className)}
       {...props}
     />
   ),
@@ -70,30 +64,26 @@ const components = {
     ...props
   }: React.ImgHTMLAttributes<HTMLImageElement>) => (
     // eslint-disable-next-line @next/next/no-img-element
-    <img
-      className={cn("rounded-md shadow-lg", className)}
-      alt={alt}
-      {...props}
-    />
+    <img className={cn("rounded-md border", className)} alt={alt} {...props} />
   ),
-  hr: (props: React.HTMLAttributes<HTMLHRElement>) => (
-    <hr className="my-6 border-t border-gray-300" {...props} />
+  hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
+    <hr className="my-4 md:my-8" {...props} />
   ),
   table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-    <div className="my-6 w-full overflow-x-auto rounded-lg shadow-sm">
+    <div className="my-6 w-full overflow-y-auto rounded-lg">
       <table
-        className={cn("w-full text-left table-auto border-collapse", className)}
+        className={cn("w-full overflow-hidden rounded-lg", className)}
         {...props}
       />
     </div>
   ),
   tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-    <tr className={cn("border-t", className)} {...props} />
+    <tr className={cn("m-0 border-t p-0", className)} {...props} />
   ),
   th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
     <th
       className={cn(
-        "border px-4 py-2 font-bold bg-gray-100 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
+        "border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right",
         className
       )}
       {...props}
@@ -102,7 +92,7 @@ const components = {
   td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
     <td
       className={cn(
-        "border px-4 py-2 [&[align=center]]:text-center [&[align=right]]:text-right",
+        "border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right",
         className
       )}
       {...props}
@@ -111,7 +101,7 @@ const components = {
   code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
     <code
       className={cn(
-        "p-6 my-4 overflow-x-auto rounded-md font-mono text-sm",
+        "px-4 py-5 my-3 text-left h-auto overflow-x-auto rounded-md",
         className
       )}
       {...props}
