3b9c53eb41621caf2722c7cb2be6a5ca9acf08d4
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-10-17 16:38:49 -0300

    feat: improve hero ui

--- app/[locale]/(lobby)/page.tsx
+++ app/[locale]/(lobby)/page.tsx
@@ -32,14 +32,17 @@ export default function HomePage({ params: { locale } }: HomePageProps) {
   return (
     <section className="flex flex-col justify-center min-h-[80dvh] gap-6 items-center text-center relative overflow-hidden">
       <div className="hidden dark:block fixed top-1/2 left-1/2 size-6/12 bg-gradient-to-br from-purple-500 via-violet-500 to-transparent opacity-10 blur-3xl transform -skew-y-6 translate-x-[-50%] translate-y-[-50%]" />
-      <div className="space-y-3 max-w-4xl relative z-10">
-        <h1 className="text-3xl font-bold md:text-4xl lg:text-6xl py-12">
+
+      <div className="max-w-4xl space-y-6">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl">
           {t.rich("title", {
             text1: (chunks) => <span className="gradient-text">{chunks}</span>,
             text2: (chunks) => <span className="gradient-text">{chunks}</span>,
           })}
         </h1>
-        <p className="text-sm text-foreground lg:text-lg">{t("subtitle")}</p>
+        <p className="mx-auto max-w-2xl text-base text-muted-foreground sm:text-lg md:text-xl">
+          {t("subtitle")}
+        </p>
       </div>
       <div className="flex items-center gap-2 z-10">
         <Link
@@ -57,7 +60,7 @@ export default function HomePage({ params: { locale } }: HomePageProps) {
           <BoltIcon className="size-4" />
         </Link>
       </div>
-      <ul className="flex gap-5 mt-6 flex-wrap justify-center relative z-10">
+      <ul className="mt-12 flex flex-wrap justify-center gap-4">
         {technologies.map((tech, index) => (
           <li key={index} className="text-sm flex items-center gap-2">
             {tech.icon}
