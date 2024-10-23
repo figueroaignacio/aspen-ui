666d14684a1b5f862bf7441228bb2b96889197df
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-10-23 14:56:21 -0300

    style: improve navbar responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,7 @@ export function Navbar() {
   const navigation = t.raw("navigation");
 
   return (
-    <header className="flex border-b-[.0625rem] border-border px-5 lg:px-10 py-3 justify-between items-center sticky top-0 left-0 backdrop-blur-sm z-50">
+    <header className="flex border-b-[.0625rem] border-border py-2 px-5 md:px-10 lg:px-20 justify-between items-center sticky top-0 left-0 backdrop-blur-sm z-50 max-w-[1580px] mx-auto w-full">
       <SiteNavbar navigation={navigation} />
       <SiteMobileNavbar navigation={navigation} />
     </header>
