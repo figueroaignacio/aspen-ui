756c9be337c94397ccb58f663364a7cb5d4a82f9
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-11 18:37:36 -0300

    chore: remove comment

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -50,7 +50,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <button
         className={cn(
           "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
-          buttonVariants(variant, size), // Usa buttonVariants aquí
+          buttonVariants(variant, size),
           className
         )}
         ref={ref}
