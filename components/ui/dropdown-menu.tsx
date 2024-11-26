c511dad73d499dd6e8d82b3ded5c7ad3e7ddb341
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-25 23:50:44 -0300

    feat: add effects to dropdown meny

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -68,7 +68,7 @@ function DropdownMenuTrigger({
       aria-expanded={false}
       aria-controls="dropdown-menu-content"
       className={cn(
-        "inline-flex justify-between w-full rounded-md border px-4 py-2 bg-primary hover:bg-primary-foreground text-sm font-medium text-foreground",
+        "inline-flex justify-between w-full rounded-md border px-4 py-2 bg-primary hover:bg-primary-foreground text-sm font-medium text-foregroundz active:scale-95 transition-all duration-200",
         className
       )}
     >
