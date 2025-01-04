a8ba0fd50bfa3dc430375ad90ab73bc1912cacb0
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-04 17:31:34 -0300

    feat: add breadcrumbs

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,19 +1,18 @@
-import { docs } from "@content";
-import { notFound } from "next/navigation";
-
-// import {
-//   Breadcrumb,
-//   BreadcrumbItem,
-//   BreadcrumbLink,
-//   BreadcrumbList,
-//   BreadcrumbSeparator,
-// } from "@/components/ui/breadcrumb";
 import { SidebarNav } from "@/components/layout/sidebar-nav";
 import { MDXContentRenderer } from "@/components/mdx/mdx-content-renderer";
 import { DashboardTableOfContents } from "@/components/mdx/toc";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { siteConfig } from "@/lib/config/siteConfig";
 import { absoluteUrl, cn } from "@/lib/utils";
+import { docs } from "@content";
 import type { Metadata } from "next";
+import { notFound } from "next/navigation";
 import Balancer from "react-wrap-balancer";
 
 type DocPageProps = {
@@ -88,41 +87,39 @@ export default async function DocPage({
   }
 
   return (
-    <main className="relative p-2 md:p-4 lg:py-6 lg:px-8 xl:grid xl:grid-cols-[200px_1fr_200px] xl:gap-10 max-w-7xl mx-auto mt-6">
-      {/* Sidebar */}
+    <main className="relative px-5 md:px-10 lg:px-20 xl:grid xl:grid-cols-[200px_1fr_200px] xl:gap-10 max-w-[1580px] mx-auto mt-6">
       <div className="hidden xl:block">
-        <div className="sticky top-16 -mt-10 h-[calc(100vh-3.5rem)] pt-4">
+        <div className="sticky top-16 -mt-5 h-[calc(100vh-3.5rem)] pt-4">
           <SidebarNav />
         </div>
       </div>
       <div className="w-full mx-auto min-w-0">
-        {/* Breadcrumb (optional) */}
-        {/* <Breadcrumb className="mb-4">
-        <BreadcrumbList>
-          {doc.slug.split("/").map((slug, index) => (
-            <div className="flex items-center gap-2" key={index}>
-              <BreadcrumbItem>
-                <BreadcrumbLink
-                  href={`/${doc.slug
-                    .split("/")
-                    .slice(0, index + 1)
-                    .join("/")}`}
-                  className={cn(
-                    index === doc.slug.split("/").length - 1
-                      ? "text-foreground"
-                      : "text-muted-foreground"
-                  )}
-                >
-                  {slug.charAt(0).toUpperCase() + slug.slice(1)}
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              {index < doc.slug.split("/").length - 1 && (
-                <BreadcrumbSeparator />
-              )}
-            </div>
-          ))}
-        </BreadcrumbList>
-      </Breadcrumb> */}
+        <Breadcrumb className="mb-4">
+          <BreadcrumbList>
+            {doc.slug.split("/").map((slug, index) => (
+              <div className="flex items-center gap-2" key={index}>
+                <BreadcrumbItem>
+                  <BreadcrumbLink
+                    href={`/${doc.slug
+                      .split("/")
+                      .slice(0, index + 1)
+                      .join("/")}`}
+                    className={cn(
+                      index === doc.slug.split("/").length - 1
+                        ? "text-foreground"
+                        : "text-muted-foreground"
+                    )}
+                  >
+                    {slug.charAt(0).toUpperCase() + slug.slice(1)}
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                {index < doc.slug.split("/").length - 1 && (
+                  <BreadcrumbSeparator />
+                )}
+              </div>
+            ))}
+          </BreadcrumbList>
+        </Breadcrumb>
         <div className="space-y-2">
           <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
             {doc.title}
@@ -137,10 +134,8 @@ export default async function DocPage({
           <MDXContentRenderer code={doc.body} />
         </div>
       </div>
-
-      {/* Table of Contents */}
       <div className="hidden xl:block">
-        <div className="sticky top-16 -mt-10 h-[calc(100vh-3.5rem)] pt-4">
+        <div className="sticky top-16 -mt-5 h-[calc(100vh-3.5rem)] pt-4">
           {doc.toc.visible && (
             <DashboardTableOfContents toc={doc.toc.content} />
           )}
