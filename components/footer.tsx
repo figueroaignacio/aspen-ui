10bead1beb5240dc63c375b0eee48992aa308f5c
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-06 17:56:39 -0300

    fix: footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,16 @@
 export function Footer() {
   return (
-    <footer className="border-t py-4 text-sm text-foreground px-5">
-      The source code is available on{" "}
-      <a
-        href="https://github.com/figueroaignacio/aspen-ui"
-        target="blank"
-        className="underline"
-      >
-        GitHub
-      </a>
+    <footer className="border-t py-4 text-sm text-foreground px-5 flex border-b md:px-10 lg:px-20  backdrop-blur-sm z-50 max-w-[1580px] mx-auto w-full">
+      <p>
+        The source code is available on{" "}
+        <a
+          href="https://github.com/figueroaignacio/aspen-ui"
+          target="blank"
+          className="underline"
+        >
+          GitHub
+        </a>
+      </p>
     </footer>
   );
 }
