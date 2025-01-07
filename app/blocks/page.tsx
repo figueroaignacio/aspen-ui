e568de1380115a8f9ecb6529c878c34a99c4b9ba
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-07 02:50:30 -0300

    fix: pages

--- app/blocks/page.tsx
+++ app/blocks/page.tsx
@@ -2,8 +2,7 @@
 import { BgBlur } from "@/components/bg-blur";
 
 // Icons
-import { CommandLineIcon } from "@heroicons/react/24/outline";
-import { CodeBracketIcon } from "@heroicons/react/24/solid";
+import { CodeIcon } from "@radix-ui/react-icons";
 
 export default function BlocksPage() {
   return (
@@ -13,16 +12,13 @@ export default function BlocksPage() {
         <div className="p-8">
           <div className="mb-6 flex justify-center">
             <div className="rounded-full bg-primary/10 p-3">
-              <CodeBracketIcon className="h-8 w-8 text-primary" />
+              <CodeIcon className="h-8 w-8" />
             </div>
           </div>
           <h1 className="mb-4 text-center text-2xl font-bold text-foreground">
             {/* {t("title")} */}
           </h1>
           <div className="rounded-lg border-[1px] border-dashed border-border p-6">
-            <div className="mb-4 flex justify-center">
-              <CommandLineIcon className="h-10 w-10 text-muted-foreground" />
-            </div>
             <p className="text-center text-sm text-foreground">
               {/* {t("description")} */}
             </p>
--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,34 +1,13 @@
 // Components
-import { BgBlur } from "@/components/bg-blur";
 
 // Icons
-import { CommandLineIcon } from "@heroicons/react/24/outline";
-import { CodeBracketIcon } from "@heroicons/react/24/solid";
+import { CodeIcon } from "@radix-ui/react-icons";
 
 export default function TemplatesPage() {
   return (
-    <section className="relative flex min-h-[80dvh] items-center justify-center p-4">
-      <BgBlur />
-      <div className="w-full max-w-md overflow-hidden rounded-xl">
-        <div className="p-8">
-          <div className="mb-6 flex justify-center">
-            <div className="rounded-full bg-primary/10 p-3">
-              <CodeBracketIcon className="h-8 w-8 text-primary" />
-            </div>
-          </div>
-          <h1 className="mb-4 text-center text-2xl font-bold text-foreground">
-            {/* {t("title")} */}
-          </h1>
-          <div className="rounded-lg border-[1px] border-dashed border-border p-6">
-            <div className="mb-4 flex justify-center">
-              <CommandLineIcon className="h-10 w-10 text-muted-foreground" />
-            </div>
-            <p className="text-center text-sm text-foreground">
-              {/* {t("description")} */}
-            </p>
-          </div>
-        </div>
-      </div>
+    <section className="relative flex flex-col min-h-[80dvh] items-center justify-center p-4">
+      <CodeIcon className="h-8 w-8 " />
+      <p>In Development</p>
     </section>
   );
 }
