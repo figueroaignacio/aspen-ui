60fc4cc16fb7d6e950a75d3b4046ceaca0a9f2fc
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-07 01:18:03 -0300

    chore: remove comments

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,17 +1,14 @@
 import { ImageResponse } from "next/og";
 
-// Image metadata
 export const size = {
   width: 32,
   height: 32,
 };
 export const contentType = "image/png";
 
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
           fontSize: 24,
