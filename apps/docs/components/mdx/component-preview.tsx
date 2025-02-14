68244aebe2df4725082845afcf4f77519fbbdf47
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-14 17:19:29 -0300

    feat: add bg grid pattern

--- apps/docs/components/mdx/component-preview.tsx
+++ apps/docs/components/mdx/component-preview.tsx
@@ -1,7 +1,10 @@
+import type React from "react";
+
 export function ComponentPreview({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center items-center border min-h-[40dvh] h-auto rounded-md">
-      <div className="text-center">{children}</div>
+    <div className="flex justify-center items-center border min-h-[40dvh] h-auto rounded-md relative overflow-hidden">
+      <div className="absolute inset-0 bg-grid-pattern" />
+      <div className="text-center relative z-10">{children}</div>
     </div>
   );
 }
