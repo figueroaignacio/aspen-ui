544e375299a6fc152a8deb8115c383c247c6588d
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-16 03:55:49 -0300

    style: add border dashed

--- apps/docs/components/footer.tsx
+++ apps/docs/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="border-t py-4 text-sm text-foreground px-5 flex border-b md:px-10 lg:px-20  backdrop-blur-sm z-50 max-w-[1580px] mx-auto w-full">
+    <footer className="border-t border-dashed py-4 text-sm text-foreground px-5 flex border-b md:px-10 lg:px-20  backdrop-blur-sm z-50 max-w-[1580px] mx-auto w-full">
       <p>
         The source code is available on{" "}
         <a
