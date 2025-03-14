9f9a777250f17eb0d4c6f0db3b33d57c0efa3257
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-14 18:47:45 -0300

    style: remove diagonal bg

--- apps/docs/app/(app)/docs/[[...slug]]/page.tsx
+++ apps/docs/app/(app)/docs/[[...slug]]/page.tsx
@@ -76,7 +76,7 @@ export default async function DocPage({
     <>
       <BgBlur />
       <div className="grid grid-cols-1 lg:grid-cols-[270px_1fr_270px] gap-6 bg-background">
-        <aside className="sticky top-14 h-[calc(100vh-3.5rem)] border-r border-dashed py-6 pl-6 hidden lg:block bg-diagonal-lines bg-background">
+        <aside className="sticky top-14 h-[calc(100vh-3.5rem)] border-r border-dashed py-6 pl-6 hidden lg:block bg-background">
           <SidebarNav />
         </aside>
 
@@ -126,7 +126,7 @@ export default async function DocPage({
         </main>
 
         {doc.toc?.visible && (
-          <aside className="sticky top-14 h-[calc(100vh-3.5rem)] border-l border-dashed py-6 pl-6 hidden md:block bg-diagonal-lines bg-background">
+          <aside className="sticky top-14 h-[calc(100vh-3.5rem)] border-l border-dashed py-6 pl-6 hidden md:block bg-background">
             <div className="text-sm">
               <Toc toc={doc.toc.content} />
             </div>
