82335bab5b0c68c429ce7a2f93286acead8dac46
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-25 20:41:19 -0300

    chore: remove github link

--- apps/docs/components/github-link.tsx
+++ apps/docs/components/github-link.tsx
@@ -10,7 +10,7 @@ export function GitHubLink() {
       className={`${buttonVariants({
         variant: "ghost",
         size: "icon",
-      })} hidden lg:flex`}
+      })}`}
     >
       <GitHubLogoIcon />
     </Link>
--- apps/docs/components/header-actions.tsx
+++ apps/docs/components/header-actions.tsx
@@ -1,5 +1,4 @@
 // Components
-import { GitHubLink } from "./github-link";
 import { LocaleSwitcher } from "./locale-switcher";
 import { Searcher } from "./searcher";
 import { ToggleTheme } from "./toggle-theme";
@@ -7,7 +6,6 @@ import { ToggleTheme } from "./toggle-theme";
 export function HeaderActions() {
   return (
     <div className="flex items-center gap-4">
-      <GitHubLink />
       <ToggleTheme />
       <LocaleSwitcher />
       <Searcher />
