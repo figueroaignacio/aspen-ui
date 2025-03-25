1489be7d6bdc6cc1dd7f24bf5bdd379f996a33fb
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-25 20:45:41 -0300

    chore: styles stuff

--- apps/docs/app/[locale]/(app)/docs/[[...slug]]/page.tsx
+++ apps/docs/app/[locale]/(app)/docs/[[...slug]]/page.tsx
@@ -2,20 +2,12 @@
 import { MDXContentRenderer } from "@/components/mdx/mdx-content-renderer";
 import { Toc } from "@/components/mdx/toc";
 import { Sidebar } from "@/components/sidebar";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import Balancer from "react-wrap-balancer";
 
 // Content
 import { docs } from "@content";
 
 // Utils
-import { cn } from "@/lib/utils";
 import { notFound } from "next/navigation";
 
 // Metadata
@@ -81,33 +73,6 @@ export default async function DocPage({
         </aside>
         <main className="min-w-0 py-6">
           <div className="mx-auto md:px-4 lg:px-0">
-            <Breadcrumb className="mb-4">
-              <BreadcrumbList>
-                {doc.slug.split("/").map((slug, index) => (
-                  <div className="flex items-center gap-2" key={index}>
-                    <BreadcrumbItem>
-                      <BreadcrumbLink
-                        href={`/docs/${doc.slug
-                          .split("/")
-                          .slice(0, index + 1)
-                          .join("/")}`}
-                        className={cn(
-                          "hover:text-foreground",
-                          index === doc.slug.split("/").length - 1
-                            ? "text-foreground"
-                            : "text-muted-foreground"
-                        )}
-                      >
-                        {slug.charAt(0).toUpperCase() + slug.slice(1)}
-                      </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    {index < doc.slug.split("/").length - 1 && (
-                      <BreadcrumbSeparator />
-                    )}
-                  </div>
-                ))}
-              </BreadcrumbList>
-            </Breadcrumb>
             <div className="space-y-2">
               <h1 className="scroll-m-20 text-2xl font-bold tracking-tight">
                 {doc.title}
@@ -124,7 +89,7 @@ export default async function DocPage({
           </div>
         </main>
         {doc.toc?.visible && (
-          <aside className="sticky top-14 h-[calc(100vh-3.5rem)] border-l border-dashed py-6 pl-6 hidden md:block">
+          <aside className="sticky top-14 h-[calc(100vh-3.5rem)] py-6 pl-6 hidden lg:block">
             <Toc toc={doc.toc.content} />
           </aside>
         )}
--- apps/docs/components/bg-blur.tsx
+++ apps/docs/components/bg-blur.tsx
@@ -2,7 +2,6 @@ export function BgBlur() {
   return (
     <>
       <div className="hidden dark:block fixed top-0 left-0 size-96 bg-gradient-to-br from-purple-500 via-violet-500 to-transparent opacity-10 blur-3xl transform -skew-y-6 z-20" />
-      <div className="hidden dark:block fixed top-96 left-[70rem] size-96 bg-gradient-to-br from-purple-500 via-violet-500 to-transparent opacity-10 blur-3xl transform -skew-y-6 -z-20" />
     </>
   );
 }
