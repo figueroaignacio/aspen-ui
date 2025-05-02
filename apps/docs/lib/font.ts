50ca6823da26c6642812107fce240faab43a297c
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-02 00:13:40 -0300

    chore: remove jetbrains font

--- apps/docs/lib/font.ts
+++ apps/docs/lib/font.ts
@@ -1,4 +1,4 @@
-import { IBM_Plex_Mono, IBM_Plex_Sans, JetBrains_Mono } from "next/font/google";
+import { IBM_Plex_Mono, IBM_Plex_Sans } from "next/font/google";
 
 export const fontSans = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -9,12 +9,5 @@ export const fontSans = IBM_Plex_Sans({
 export const fontMono = IBM_Plex_Mono({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
-  display: "swap",
-});
-
-export const jetbrains = JetBrains_Mono({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
-  style: ["normal"],
-  subsets: ["latin"],
   display: "swap",
 });
