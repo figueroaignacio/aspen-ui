5eb3c15fa49b5a62994c3016af679ee9e4af3ff3
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-07 22:44:27 -0300

    styles: add hover animation to code block component

--- apps/docs/components/mdx/codeblock.tsx
+++ apps/docs/components/mdx/codeblock.tsx
@@ -10,13 +10,13 @@ export const CodeBlock = ({
   ...props
 }: React.HTMLAttributes<HTMLPreElement>) => {
   return (
-    <div className="relative rounded-xl font-mono text-sm">
-      <div className="absolute top-3 right-3 z-10">
+    <div className="relative rounded-xl font-mono text-sm group">
+      <div className="absolute top-3 right-3 z-10 opacity-0 transition-opacity group-hover:opacity-100">
         <CopyButton>{children}</CopyButton>
       </div>
       <div className="w-full p-[2px]">
         <pre
-          className={cn("overflow-x-auto  rounded-xl font-mono", className)}
+          className={cn("overflow-x-auto rounded-xl font-mono", className)}
           {...props}
         >
           <code>{children}</code>
